0f98797b55f190becdbd19273e08288646242d56
Mizael Pinto Almeida <mzalmeida.a@gmail.com>
2022-11-13 19:02:14 -0300

    feat: added models

---
+++ CnpChallenge.Domain/Model/ClienteModel/ClienteEndereco.cs
@@ -0,0 +1,16 @@
+﻿using CnpChallenge.Domain.Enum;
+
+namespace CnpChallenge.Domain.Model.ClienteModel;
+
+public class ClienteEndereco : _ModelBase
+{
+    public int IdCliente { get; set; }
+    public string Logradouro { get; set; }
+    public string Cep { get; set; }
+    public string Uf { get; set; }
+    public string Cidade { get; set; }
+    public string Bairro { get; set; }
+    public EStatusCadastro Status { get; set; }
+    
+    public virtual Cliente Cliente { get; set; }
+}
