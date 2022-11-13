8fce0dfbb3d8de1a9cab75371276b5f8b87e9e4a
Mizael Pinto Almeida <mzalmeida.a@gmail.com>
2022-11-13 19:02:58 -0300

    feat: added config for models

---
+++ CnpChallenge.Infrastructure/ModelConfig/ClienteConfig.cs
@@ -0,0 +1,28 @@
+﻿using CnpChallenge.Domain.Model.ClienteModel;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CnpChallenge.Infrastructure.ModelConfig;
+
+public class ClienteConfig : IEntityTypeConfiguration<Cliente>
+{
+    public void Configure(EntityTypeBuilder<Cliente> builder)
+    {
+        builder
+            .ToTable(nameof(Cliente), "dbo")
+            .HasKey(c => c.Id)
+            .IsClustered();
+
+        builder.Property(c => c.Nome).IsRequired().HasMaxLength(200);
+        builder.Property(c => c.DtNascimento).IsRequired();
+        builder.Property(c => c.Status).IsRequired().HasColumnType("tinyint");
+        builder.Property(c => c.DatInclusao).IsRequired().HasDefaultValueSql("getdate()").ValueGeneratedOnAdd();
+
+        builder
+            .HasMany(c => c.Enderecos)
+            .WithOne(e => e.Cliente)
+            .HasForeignKey(e => e.IdCliente)
+            .OnDelete(DeleteBehavior.Cascade);
+
+    }
+}
---
+++ CnpChallenge.Infrastructure/ModelConfig/ClienteEnderecoConfig.cs
@@ -0,0 +1,24 @@
+﻿using CnpChallenge.Domain.Model.ClienteModel;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CnpChallenge.Infrastructure.ModelConfig;
+
+public class ClienteEnderecoConfig : IEntityTypeConfiguration<ClienteEndereco>
+{
+    public void Configure(EntityTypeBuilder<ClienteEndereco> builder)
+    {
+        builder
+            .ToTable(nameof(ClienteEndereco), "dbo")
+            .HasKey(e => e.Id)
+            .IsClustered();
+
+        builder.Property(e => e.Logradouro).IsRequired().HasMaxLength(100);
+        builder.Property(e => e.Cep).IsRequired().HasMaxLength(8);
+        builder.Property(e => e.Uf).IsRequired().HasMaxLength(2);
+        builder.Property(e => e.Cidade).IsRequired().HasMaxLength(100);
+        builder.Property(e => e.Bairro).IsRequired().HasMaxLength(60);
+        builder.Property(e => e.Status).IsRequired().HasColumnType("tinyint");
+        builder.Property(e => e.DatInclusao).HasDefaultValueSql("getdate()").ValueGeneratedOnAdd();
+    }
+}
