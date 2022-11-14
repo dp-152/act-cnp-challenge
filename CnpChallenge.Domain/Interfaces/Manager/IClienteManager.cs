811faa5e2bdfc9ce13f440cda37580e3b21fe4cb
Mizael Pinto Almeida <mzalmeida.a@gmail.com>
2022-11-14 14:25:41 -0300

    feat: added interface for Cliente manager

---
+++ CnpChallenge.Domain/Interfaces/Manager/IClienteManager.cs
@@ -0,0 +1,11 @@
+﻿using CnpChallenge.Domain.DTO.Manager;
+using CnpChallenge.Domain.Manager;
+using CnpChallenge.Domain.Model.ClienteModel;
+
+namespace CnpChallenge.Domain.Interfaces.Manager;
+
+public interface IClienteManager
+{
+    Task<Cliente> Create(ClienteManagerCreateRequest request);
+    Task<Cliente> Update(ClienteManagerUpdateRequest request);
+}
