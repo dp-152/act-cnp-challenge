4bc04431832d8b0394904e56d175f35b70bb07d4
Mizael Pinto Almeida <mzalmeida.a@gmail.com>
2022-11-14 04:05:02 -0300

    feat: implemented GetAll

--- CnpChallenge.Application/Feature/Cliente/ClienteServices.GetAll.cs
+++ CnpChallenge.Application/Feature/Cliente/ClienteServices.GetAll.cs
@@ -7,6 +7,24 @@ public partial class ClienteServices
 {
     public async Task<IEnumerable<ClienteGetAllResponse>> GetAllClientes()
     {
-        throw new NotImplementedException();
+        var result = await _clienteRepository.GetAll();
+
+        return result.Select(c => new ClienteGetAllResponse
+        {
+            Id = c.Id,
+            Addresses = c.Enderecos.Select(e => new ClienteEnderecoResponseBase
+            {
+                Id = e.Id,
+                Address = e.Logradouro,
+                City = e.Cidade,
+                District = e.Bairro,
+                State = e.Uf,
+                Status = e.Status,
+                ZipCode = e.Cep,
+            }),
+            BirthDate = c.DtNascimento,
+            Name = c.Nome,
+            Status = c.Status,
+        });
     }
 }
