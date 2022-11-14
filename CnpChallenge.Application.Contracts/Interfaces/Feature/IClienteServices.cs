c53db2584826fa14289a680b71ac2918a4dae2a9
Mizael Pinto Almeida <mzalmeida.a@gmail.com>
2022-11-14 04:00:15 -0300

    feat: created interface for cliente services

---
+++ CnpChallenge.Application.Contracts/Interfaces/Feature/IClienteServices.cs
@@ -0,0 +1,12 @@
+﻿using CnpChallenge.Application.Contracts.DTO.Feature.ClienteServices;
+
+namespace CnpChallenge.Application.Contracts.Interfaces.Feature;
+
+public interface IClienteServices
+{
+    Task<ClienteCreateResponse> CreateCliente(ClienteCreateCommand command);
+    Task<ClienteDeleteResponse> DeleteCliente(ClienteDeleteCommand command);
+    Task<IEnumerable<ClienteGetAllResponse>> GetAllClientes();
+    Task<ClienteGetResponse?> GetCliente(ClienteGetRequest request);
+    Task<ClienteUpdateResponse> UpdateCliente(ClienteUpdateCommand command);
+}
