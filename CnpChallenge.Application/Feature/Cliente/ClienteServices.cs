dca1201c6cd072ebab43d7c2a3e981cc9e88a651
Mizael Pinto Almeida <mzalmeida.a@gmail.com>
2022-11-14 14:35:31 -0300

    feat: added manager to service

--- CnpChallenge.Application/Feature/Cliente/ClienteServices.cs
+++ CnpChallenge.Application/Feature/Cliente/ClienteServices.cs
@@ -1,4 +1,5 @@
 ﻿using CnpChallenge.Application.Contracts.Interfaces.Feature;
+using CnpChallenge.Domain.Interfaces.Manager;
 using CnpChallenge.Domain.Interfaces.Repository;
 
 namespace CnpChallenge.Application.Feature.Cliente;
@@ -6,9 +7,11 @@ namespace CnpChallenge.Application.Feature.Cliente;
 public partial class ClienteServices : IClienteServices
 {
     private readonly IClienteRepository _clienteRepository;
+    private readonly IClienteManager _clienteManager;
 
-    public ClienteServices(IClienteRepository clienteRepository)
+    public ClienteServices(IClienteRepository clienteRepository, IClienteManager clienteManager)
     {
         _clienteRepository = clienteRepository ?? throw new ArgumentNullException(nameof(clienteRepository));
+        _clienteManager = clienteManager ?? throw new ArgumentNullException(nameof(clienteManager));
     }
 }
