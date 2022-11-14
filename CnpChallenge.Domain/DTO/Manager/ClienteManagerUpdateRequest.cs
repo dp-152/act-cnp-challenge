826c3f655bf9344825ff05bc9214c08dce7a9dd4
Mizael Pinto Almeida <mzalmeida.a@gmail.com>
2022-11-14 14:34:13 -0300

    feat: added DTOs for manager requests

---
+++ CnpChallenge.Domain/DTO/Manager/ClienteManagerUpdateRequest.cs
@@ -0,0 +1,20 @@
+﻿using CnpChallenge.Domain.Shared.Enum;
+
+namespace CnpChallenge.Domain.DTO.Manager;
+
+public class ClienteManagerUpdateRequest
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public DateTime DtNascimento { get; set; }
+    public IEnumerable<ClienteManagerUpdateRequestEndereco> Enderecos { get; set; }
+}
+
+public class ClienteManagerUpdateRequestEndereco
+{
+    public string Logradouro { get; set; }
+    public string Cep { get; set; }
+    public string Uf { get; set; }
+    public string Cidade { get; set; }
+    public string Bairro { get; set; }
+}
