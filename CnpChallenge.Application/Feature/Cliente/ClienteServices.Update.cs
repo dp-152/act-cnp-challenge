3e2cdfcc16d6bcae6fc827f36f035fd7440ed5fa
Mizael Pinto Almeida <mzalmeida.a@gmail.com>
2022-11-14 22:46:11 -0300

    feat: implemented update command

--- CnpChallenge.Application/Feature/Cliente/ClienteServices.Update.cs
+++ CnpChallenge.Application/Feature/Cliente/ClienteServices.Update.cs
@@ -1,4 +1,6 @@
-﻿using CnpChallenge.Application.Contracts.DTO.Feature.ClienteServices;
+﻿using CnpChallenge.Application.Contracts.Common.ClienteTypes;
+using CnpChallenge.Application.Contracts.DTO.Feature.ClienteServices;
+using CnpChallenge.Domain.DTO.Manager;
 
 namespace CnpChallenge.Application.Feature.Cliente;
 
@@ -6,6 +8,38 @@ public partial class ClienteServices
 {
     public async Task<ClienteUpdateResponse> UpdateCliente(ClienteUpdateCommand command)
     {
-        throw new NotImplementedException();
+        var request = new ClienteManagerUpdateRequest()
+        {
+            Nome = command.Name,
+            DtNascimento = command.BirthDate,
+            Enderecos = command.Addresses?.Select(e => new ClienteManagerUpdateRequestEndereco()
+            {
+                Bairro = e.District,
+                Cep = e.ZipCode,
+                Cidade = e.City,
+                Logradouro = e.Address,
+                Uf = e.State
+            })
+        };
+
+        var createdObject = await _clienteManager.Update(request);
+        var result = await _clienteRepository.Update(createdObject);
+        
+        return new ClienteUpdateResponse() {
+            Id = result.Id,
+            Name = result.Nome,
+            Addresses = result.Enderecos.Select(e => new ClienteEnderecoResponseBase
+            {
+                Id = e.Id,
+                Address = e.Logradouro,
+                City = e.Cidade,
+                District = e.Bairro,
+                State = e.Uf,
+                Status = e.Status,
+                ZipCode = e.Cep,
+            }),
+            Status = result.Status,
+            BirthDate = result.DtNascimento,
+        };
     }
 }
