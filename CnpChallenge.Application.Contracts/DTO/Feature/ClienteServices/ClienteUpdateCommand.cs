b88bb21f15449094952304546599b6c2f29578d3
Mizael Pinto Almeida <mzalmeida.a@gmail.com>
2022-11-14 22:45:23 -0300

    feat: updated DTO for update command

--- CnpChallenge.Application.Contracts/DTO/Feature/ClienteServices/ClienteUpdateCommand.cs
+++ CnpChallenge.Application.Contracts/DTO/Feature/ClienteServices/ClienteUpdateCommand.cs
@@ -1,14 +1,19 @@
-﻿using CnpChallenge.Application.Contracts.Common.ClienteTypes;
+﻿namespace CnpChallenge.Application.Contracts.DTO.Feature.ClienteServices;
 
-namespace CnpChallenge.Application.Contracts.DTO.Feature.ClienteServices;
-
-public class ClienteUpdateCommand : ClienteBase
+public class ClienteUpdateCommand
 {
     public int Id { get; set; }
-    public IEnumerable<ClienteUpdateCommandEndereco> Addresses { get; set; }
+    public string? Name { get; set; }
+    public DateTime? BirthDate { get; set; }
+    public IEnumerable<ClienteUpdateCommandEndereco>? Addresses { get; set; }
 }
 
-public class ClienteUpdateCommandEndereco : ClienteEnderecoBase
+public class ClienteUpdateCommandEndereco
 {
     public int? Id { get; set; }
+    public string? Address { get; set; }
+    public string? ZipCode { get; set; }
+    public string? State { get; set; }
+    public string? City { get; set; }
+    public string? District { get; set; }
 }
