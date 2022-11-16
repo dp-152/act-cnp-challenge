3a1359a0f442cbe948c71e076d890ac2afc34316
Mizael Pinto Almeida <mzalmeida.a@gmail.com>
2022-11-16 02:05:32 -0300

    feat: added newtonsoft json options

--- CnpChallenge.API/Program.cs
+++ CnpChallenge.API/Program.cs
@@ -8,12 +8,22 @@ using CnpChallenge.Infrastructure.Context;
 using CnpChallenge.Infrastructure.Repository;
 using FluentValidation;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services
+    .AddControllers()
+    .AddNewtonsoftJson(options =>
+    {
+        options.SerializerSettings.Formatting = Formatting.Indented;
+        options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+        options.SerializerSettings.DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate;
+        options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+    });
 builder.Services.AddDbContext<MainContext>((_, optionsBuilder) =>
 {
     optionsBuilder
