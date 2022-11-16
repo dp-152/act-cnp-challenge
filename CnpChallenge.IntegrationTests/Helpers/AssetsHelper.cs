94a4198b39cfaecb417278ea380caa6efae40425
Mizael Pinto Almeida <mzalmeida.a@gmail.com>
2022-11-16 20:49:54 -0300

    feat: added helper for loading assets from disk

---
+++ CnpChallenge.IntegrationTests/Helpers/AssetsHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using Newtonsoft.Json;
+
+namespace CnpChallenge.IntegrationTests.Helpers;
+
+public static class AssetsHelper
+{
+    public static async Task<TData> LoadJsonAsset<TData>(string assetName)
+    {
+        var assetPath = Path.Join(
+            Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
+            "Assets", $"{assetName}.json"
+        );
+
+        using var reader = new StreamReader(assetPath);
+        var stringContent = await reader.ReadToEndAsync() ??
+                            throw new Exception($"Cannot read contents of asset file {assetName}.json");
+
+        var deserializedObject = JsonConvert.DeserializeObject<TData>(stringContent) ??
+                                 throw new Exception($"Cannot deserialize contents of asset {assetName}.json");
+
+        return deserializedObject;
+    }
+}
