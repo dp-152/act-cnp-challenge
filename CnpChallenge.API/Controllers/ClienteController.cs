e112518de2de9eead2690c17f14cd557541a2d17
Mizael Pinto Almeida <mzalmeida.a@gmail.com>
2022-11-16 03:39:56 -0300

    feat: added controller for create

--- CnpChallenge.API/Controllers/ClienteController.cs
+++ CnpChallenge.API/Controllers/ClienteController.cs
@@ -25,11 +25,19 @@ public class ClienteController : _BaseController
         return Ok(_mapper.Map<IEnumerable<ClienteResponse>, IEnumerable<ClienteResponseDto>>(result));
     }
 
-    [HttpGet("{id}")]
+    [HttpGet("{id}", Name = "GetById")]
     [ProducesDefaultResponseType(typeof(ClienteResponseDto))]
     public async Task<IActionResult> GetById([FromRoute] int id)
     {
         var result = await _clienteServices.GetCliente(new ClienteGetRequest { Id = id });
         return Ok(_mapper.Map<ClienteResponseDto>(result));
+    }
+
+    [HttpPost]
+    [ProducesDefaultResponseType(typeof(ClienteResponseDto))]
+    public async Task<IActionResult> Create([FromBody] ClienteRequestDto request)
+    {
+        var result = await _clienteServices.CreateCliente(_mapper.Map<ClienteCreateCommand>(request));
+        return CreatedAtRoute("GetById", new { id = result.Id.ToString() }, _mapper.Map<ClienteResponseDto>(result));
     }
 }
