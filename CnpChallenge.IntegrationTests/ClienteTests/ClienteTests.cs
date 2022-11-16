43e76696a79d68e0b59c295ddc500a844fb2003f
Mizael Pinto Almeida <mzalmeida.a@gmail.com>
2022-11-16 20:13:40 -0300

    feat: created initial test

---
+++ CnpChallenge.IntegrationTests/ClienteTests/ClienteTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Mime;
+using CnpChallenge.IntegrationTests.Extensions.WebApplicationFactoryExtensions;
+using FluentAssertions;
+
+namespace CnpChallenge.IntegrationTests.ClienteTests;
+
+public class ClienteTests : IClassFixture<CustomWebApplicationFactory<Program>>
+{
+    private readonly CustomWebApplicationFactory<Program> _factory;
+    private readonly HttpClient _client;
+
+    public ClienteTests(CustomWebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+        _client = _factory.CreateClient();
+    }
+
+    [Theory]
+    [InlineData("/api/v1/cliente")]
+    public async Task Get_EndpointReturnsSuccessAndCorrectContentType(string url)
+    {
+        var response = await _client.GetAsync(url);
+
+        response.EnsureSuccessStatusCode();
+
+        response?.Content?.Headers?.ContentType?.ToString().Should().Contain(MediaTypeNames.Application.Json);
+    }
+}
