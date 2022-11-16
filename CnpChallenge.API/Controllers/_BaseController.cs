e4d6351860b0d9a0ccbcf3b2c79cb1fa30f281b8
Mizael Pinto Almeida <mzalmeida.a@gmail.com>
2022-11-16 01:59:31 -0300

    feat: added base controller

---
+++ CnpChallenge.API/Controllers/_BaseController.cs
@@ -0,0 +1,10 @@
+﻿using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CnpChallenge.API.Controllers;
+
+[ApiController]
+[Route("/api/[controller]")]
+[Produces(MediaTypeNames.Application.Json)]
+public class _BaseController : Controller
+{ }
