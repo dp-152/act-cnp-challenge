60463aac16058db36bbc6ffa48921d46d09540db
Mizael Pinto Almeida <mzalmeida.a@gmail.com>
2022-11-17 18:41:10 -0300

    feat: added test suite for SQL Server tests

---
+++ CnpChallenge.IntegrationTests/ClienteTests/ClienteTestsSqlServer.cs
@@ -0,0 +1,27 @@
+﻿using CnpChallenge.Infrastructure.Context;
+using CnpChallenge.IntegrationTests.Extensions.WebApplicationFactoryExtensions;
+using CnpChallenge.IntegrationTests.Helpers;
+
+namespace CnpChallenge.IntegrationTests.ClienteTests;
+
+public partial class ClienteTestsSqlServer : IClassFixture<SqlServerWebApplicationFactory<Program>>
+{
+    
+    private readonly SqlServerWebApplicationFactory<Program> _factory;
+    private readonly HttpClient _client;
+
+    public ClienteTestsSqlServer(SqlServerWebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+        _client = _factory.CreateClient();
+    }
+
+    private async Task SeedDatabase(string dataset)
+    {
+        using (var scope = _factory.Services.CreateScope())
+        {
+            var context = scope.ServiceProvider.GetRequiredService<MainContext>();
+            await DbSeedingHelper.ResetClientes(context, dataset);
+        }
+    }
+}
