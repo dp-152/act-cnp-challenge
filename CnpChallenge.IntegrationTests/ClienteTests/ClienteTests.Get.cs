83958c6a6d7ede50e315acecc408031dee467833
Mizael Pinto Almeida <mzalmeida.a@gmail.com>
2022-11-16 23:38:23 -0300

    refactor: split tests class

---
+++ CnpChallenge.IntegrationTests/ClienteTests/ClienteTests.Get.cs
@@ -0,0 +1,37 @@
+﻿using CnpChallenge.API.DTO.Cliente;
+using CnpChallenge.Domain.Model.ClienteModel;
+using CnpChallenge.Infrastructure.Context;
+using CnpChallenge.IntegrationTests.Helpers;
+using FluentAssertions;
+using Newtonsoft.Json;
+
+namespace CnpChallenge.IntegrationTests.ClienteTests;
+
+public partial class ClienteTests
+{
+    [Fact]
+    public async Task Get_EndpointReturnsValidData()
+    {
+        using (var scope = _factory.Services.CreateScope())
+        {
+            var context = scope.ServiceProvider.GetRequiredService<MainContext>();
+            await DbSeedingHelper.ResetClientes(context, "clientes_dataset_skim");
+        }
+
+        var response = await _client.GetAsync("/api/v1/cliente/6");
+        response.EnsureSuccessStatusCode();
+
+        var content = JsonConvert.DeserializeObject<ClienteResponseDto>(await response.Content.ReadAsStringAsync());
+
+        var comparisonSubject = await AssetsHelper.LoadJsonAsset<Cliente>("clienteid_6_data");
+
+        content.Should().NotBeNull();
+
+        content!.Id.Should().Be(6);
+        content.Should().BeEquivalentTo(comparisonSubject, config =>
+            config
+                .Excluding(c => c.Path.EndsWith("ClienteId"))
+                .Excluding(c => c.Path.EndsWith("Cliente"))
+        );
+    }
+}
--- CnpChallenge.IntegrationTests/ClienteTests/ClienteTests.cs
+++ CnpChallenge.IntegrationTests/ClienteTests/ClienteTests.cs
@@ -1,15 +1,12 @@
 ﻿using System.Net.Mime;
-using CnpChallenge.API.DTO.Cliente;
-using CnpChallenge.Domain.Model.ClienteModel;
 using CnpChallenge.Infrastructure.Context;
 using CnpChallenge.IntegrationTests.Extensions.WebApplicationFactoryExtensions;
 using CnpChallenge.IntegrationTests.Helpers;
 using FluentAssertions;
-using Newtonsoft.Json;
 
 namespace CnpChallenge.IntegrationTests.ClienteTests;
 
-public class ClienteTests : IClassFixture<CustomWebApplicationFactory<Program>>
+public partial class ClienteTests : IClassFixture<CustomWebApplicationFactory<Program>>
 {
     private readonly CustomWebApplicationFactory<Program> _factory;
     private readonly HttpClient _client;
@@ -29,32 +26,5 @@ public class ClienteTests : IClassFixture<CustomWebApplicationFactory<Program>>
         response.EnsureSuccessStatusCode();
 
         response?.Content?.Headers?.ContentType?.ToString().Should().Contain(MediaTypeNames.Application.Json);
-    }
-
-    [Fact]
-    public async Task Get_EndpointReturnsValidData()
-    {
-        using (var scope = _factory.Services.CreateScope())
-        {
-            var context = scope.ServiceProvider.GetRequiredService<MainContext>();
-
-            await DbSeedingHelper.SeedClientes(context, "clientes_dataset_skim");
-        }
-
-        var response = await _client.GetAsync("/api/v1/cliente/6");
-        response.EnsureSuccessStatusCode();
-
-        var content = JsonConvert.DeserializeObject<ClienteResponseDto>(await response.Content.ReadAsStringAsync());
-
-        var comparisonSubject = await AssetsHelper.LoadJsonAsset<Cliente>("clienteid_6_data");
-
-        content.Should().NotBeNull();
-
-        content!.Id.Should().Be(6);
-        content.Should().BeEquivalentTo(comparisonSubject, config => 
-            config
-                .Excluding(c => c.Path.EndsWith("ClienteId"))
-                .Excluding(c => c.Path.EndsWith("Cliente"))
-            );
     }
 }
