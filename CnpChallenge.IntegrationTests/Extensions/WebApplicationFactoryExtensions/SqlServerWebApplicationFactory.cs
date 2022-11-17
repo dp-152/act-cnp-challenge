ce1246638b3a2b27b7fdad35cf95878472241204
Mizael Pinto Almeida <mzalmeida.a@gmail.com>
2022-11-17 18:40:42 -0300

    feat: added WebApplicationFactory for SQL Server connection

---
+++ CnpChallenge.IntegrationTests/Extensions/WebApplicationFactoryExtensions/SqlServerWebApplicationFactory.cs
@@ -0,0 +1,33 @@
+﻿using CnpChallenge.Infrastructure.Context;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+
+namespace CnpChallenge.IntegrationTests.Extensions.WebApplicationFactoryExtensions;
+
+public class SqlServerWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
+{
+    private IConfiguration? _configuration;
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureAppConfiguration(configBuilder =>
+        {
+            _configuration = configBuilder.Build();
+        });
+
+        builder.ConfigureServices(services =>
+        {
+            var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<MainContext>));
+            if (descriptor is not null) services.Remove(descriptor);
+            
+            services.AddDbContext<MainContext>((_, optionsBuilder) =>
+            {
+                optionsBuilder
+                    .UseLazyLoadingProxies()
+                    .UseSqlServer(_configuration!.GetConnectionString("Testing"));
+            });
+        });
+
+        builder.UseEnvironment("Development");
+    }
+}
