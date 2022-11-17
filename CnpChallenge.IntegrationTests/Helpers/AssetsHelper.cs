82e9d8535dc94d2de3c4d11fdd7f00df4e8636e6
Mizael Pinto Almeida <mzalmeida.a@gmail.com>
2022-11-17 03:05:59 -0300

    feat: added method to read files as strings

--- CnpChallenge.IntegrationTests/Helpers/AssetsHelper.cs
+++ CnpChallenge.IntegrationTests/Helpers/AssetsHelper.cs
@@ -5,20 +5,23 @@ namespace CnpChallenge.IntegrationTests.Helpers;
 
 public static class AssetsHelper
 {
+    private static string ResolveAssetPath (string assetName) => Path.Join(
+        Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
+        "Assets", $"{assetName}.json"
+    );
     public static async Task<TData> LoadJsonAsset<TData>(string assetName)
     {
-        var assetPath = Path.Join(
-            Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
-            "Assets", $"{assetName}.json"
-        );
-
-        using var reader = new StreamReader(assetPath);
-        var stringContent = await reader.ReadToEndAsync() ??
-                            throw new Exception($"Cannot read contents of asset file {assetName}.json");
-
+        var stringContent = await LoadJsonAssetAsString(assetName);
         var deserializedObject = JsonConvert.DeserializeObject<TData>(stringContent) ??
                                  throw new Exception($"Cannot deserialize contents of asset {assetName}.json");
 
         return deserializedObject;
+    }
+
+    public static async Task<string> LoadJsonAssetAsString(string assetName)
+    {
+        using var reader = new StreamReader(ResolveAssetPath(assetName));
+        return await reader.ReadToEndAsync() ??
+                            throw new Exception($"Cannot read contents of asset file {assetName}.json");
     }
 }
